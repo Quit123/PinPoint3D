3d89dbd1f4278c9bc79c2d405920b726101da499
Quit123 <2048751307@qq.com>
2025-11-25 17:25:38 +0800

    clean val

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 python ../evaluate.py --scan_folder=../data/PartScan \
                --val_list=../data/val_list.json \
-               --output_dir=../results/PartScan_val \
+               --output_dir=../results/PartScan \
                --checkpoint=../weights/checkpoint1099.pth
